4b886a61f588092d8d70087dc03644a5b1222355
seungwan <kweyom6@gmail.com>
2026-04-04 17:53:59 +0900

    [Engine] Fix TextureParser COM lifetime teardown

--- DomainExpansion/Engine/Code/Engine/Module/TextureParser/TextureParser.cpp
+++ DomainExpansion/Engine/Code/Engine/Module/TextureParser/TextureParser.cpp
@@ -139,124 +139,99 @@ bool TextureParser::decodeFromFile(
 		return false;
 	}
 
-	com_pointer<IWICImagingFactory> wicFactory = {};
-	const HRESULT factoryResult = CoCreateInstance(
-		CLSID_WICImagingFactory,
-		nullptr,
-		CLSCTX_INPROC_SERVER,
-		__uuidof(IWICImagingFactory),
-		reinterpret_cast<void**>(wicFactory.ReleaseAndGetAddressOf()));
-	if (FAILED(factoryResult) || wicFactory == nullptr)
-	{
-		if (shouldUninitializeCOM)
-		{
-			CoUninitialize();
-		}
-
-		outErrorText = "parse_failed";
-		return false;
-	}
-
-	const wstring textureFilePathWide = filesystem_path(resolvedTextureFilePath).wstring();
-	com_pointer<IWICBitmapDecoder> bitmapDecoder = {};
-	const HRESULT decoderResult = wicFactory->CreateDecoderFromFilename(
-		textureFilePathWide.c_str(),
-		nullptr,
-		GENERIC_READ,
-		WICDecodeMetadataCacheOnDemand,
-		bitmapDecoder.ReleaseAndGetAddressOf());
-	if (FAILED(decoderResult) || bitmapDecoder == nullptr)
-	{
-		if (shouldUninitializeCOM)
-		{
-			CoUninitialize();
-		}
-
-		outErrorText = "file_open_failed";
-		return false;
-	}
-
-	com_pointer<IWICBitmapFrameDecode> bitmapFrame = {};
-	const HRESULT frameResult = bitmapDecoder->GetFrame(0, bitmapFrame.ReleaseAndGetAddressOf());
-	if (FAILED(frameResult) || bitmapFrame == nullptr)
-	{
-		if (shouldUninitializeCOM)
-		{
-			CoUninitialize();
-		}
-
-		outErrorText = "parse_failed";
-		return false;
-	}
-
+	const uint32 channelCount = 4;
 	uint32 textureWidth = 0;
 	uint32 textureHeight = 0;
-	const HRESULT sizeResult = bitmapFrame->GetSize(&textureWidth, &textureHeight);
-	if (FAILED(sizeResult) || textureWidth == 0 || textureHeight == 0)
+	const bool decodeSucceeded = [&]()
 	{
-		if (shouldUninitializeCOM)
+		com_pointer<IWICImagingFactory> wicFactory = {};
+		const HRESULT factoryResult = CoCreateInstance(
+			CLSID_WICImagingFactory,
+			nullptr,
+			CLSCTX_INPROC_SERVER,
+			__uuidof(IWICImagingFactory),
+			reinterpret_cast<void**>(wicFactory.ReleaseAndGetAddressOf()));
+		if (FAILED(factoryResult) || wicFactory == nullptr)
 		{
-			CoUninitialize();
+			outErrorText = "parse_failed";
+			return false;
 		}
 
-		outErrorText = "parse_failed";
-		return false;
-	}
-
-	com_pointer<IWICFormatConverter> formatConverter = {};
-	const HRESULT converterCreateResult = wicFactory->CreateFormatConverter(formatConverter.ReleaseAndGetAddressOf());
-	if (FAILED(converterCreateResult) || formatConverter == nullptr)
-	{
-		if (shouldUninitializeCOM)
+		const wstring textureFilePathWide = filesystem_path(resolvedTextureFilePath).wstring();
+		com_pointer<IWICBitmapDecoder> bitmapDecoder = {};
+		const HRESULT decoderResult = wicFactory->CreateDecoderFromFilename(
+			textureFilePathWide.c_str(),
+			nullptr,
+			GENERIC_READ,
+			WICDecodeMetadataCacheOnDemand,
+			bitmapDecoder.ReleaseAndGetAddressOf());
+		if (FAILED(decoderResult) || bitmapDecoder == nullptr)
 		{
-			CoUninitialize();
+			outErrorText = "file_open_failed";
+			return false;
 		}
 
-		outErrorText = "parse_failed";
-		return false;
-	}
-
-	const HRESULT converterInitializeResult = formatConverter->Initialize(
-		bitmapFrame.Get(),
-		GUID_WICPixelFormat32bppRGBA,
-		WICBitmapDitherTypeNone,
-		nullptr,
-		0.0f,
-		WICBitmapPaletteTypeCustom);
-	if (FAILED(converterInitializeResult))
-	{
-		if (shouldUninitializeCOM)
+		com_pointer<IWICBitmapFrameDecode> bitmapFrame = {};
+		const HRESULT frameResult = bitmapDecoder->GetFrame(0, bitmapFrame.ReleaseAndGetAddressOf());
+		if (FAILED(frameResult) || bitmapFrame == nullptr)
 		{
-			CoUninitialize();
+			outErrorText = "parse_failed";
+			return false;
 		}
 
-		outErrorText = "parse_failed";
-		return false;
-	}
-
-	const uint32 channelCount = 4;
-	const uint32 rowPitchInBytes = textureWidth * channelCount;
-	vector<char>& pixelData = outTextureAsset.getPixelData();
-	pixelData.resize(static_cast<size_t>(rowPitchInBytes) * static_cast<size_t>(textureHeight));
-	const HRESULT copyPixelsResult = formatConverter->CopyPixels(
-		nullptr,
-		rowPitchInBytes,
-		static_cast<uint32>(pixelData.size()),
-		reinterpret_cast<BYTE*>(pixelData.data()));
-	if (FAILED(copyPixelsResult))
-	{
-		if (shouldUninitializeCOM)
+		const HRESULT sizeResult = bitmapFrame->GetSize(&textureWidth, &textureHeight);
+		if (FAILED(sizeResult) || textureWidth == 0 || textureHeight == 0)
 		{
-			CoUninitialize();
+			outErrorText = "parse_failed";
+			return false;
 		}
 
-		outErrorText = "parse_failed";
-		return false;
-	}
+		com_pointer<IWICFormatConverter> formatConverter = {};
+		const HRESULT converterCreateResult = wicFactory->CreateFormatConverter(formatConverter.ReleaseAndGetAddressOf());
+		if (FAILED(converterCreateResult) || formatConverter == nullptr)
+		{
+			outErrorText = "parse_failed";
+			return false;
+		}
+
+		const HRESULT converterInitializeResult = formatConverter->Initialize(
+			bitmapFrame.Get(),
+			GUID_WICPixelFormat32bppRGBA,
+			WICBitmapDitherTypeNone,
+			nullptr,
+			0.0f,
+			WICBitmapPaletteTypeCustom);
+		if (FAILED(converterInitializeResult))
+		{
+			outErrorText = "parse_failed";
+			return false;
+		}
+
+		const uint32 rowPitchInBytes = textureWidth * channelCount;
+		vector<char>& pixelData = outTextureAsset.getPixelData();
+		pixelData.resize(static_cast<size_t>(rowPitchInBytes) * static_cast<size_t>(textureHeight));
+		const HRESULT copyPixelsResult = formatConverter->CopyPixels(
+			nullptr,
+			rowPitchInBytes,
+			static_cast<uint32>(pixelData.size()),
+			reinterpret_cast<BYTE*>(pixelData.data()));
+		if (FAILED(copyPixelsResult))
+		{
+			outErrorText = "parse_failed";
+			return false;
+		}
+
+		return true;
+	}();
 
 	if (shouldUninitializeCOM)
 	{
 		CoUninitialize();
+	}
+
+	if (!decodeSucceeded)
+	{
+		return false;
 	}
 
 	outTextureAsset.setWidth(textureWidth);
